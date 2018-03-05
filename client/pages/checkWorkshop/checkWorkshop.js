cb1dbfbfaf79a38af0bd699a5f67dec358bd4176
Xv Tao <1099869716@qq.com>
2018-03-05 14:39:02 +0800

    修复提交发现隐患报告不能提交bug

--- client/pages/checkWorkshop/checkWorkshop.js
+++ client/pages/checkWorkshop/checkWorkshop.js
@@ -199,7 +199,38 @@ Page({
         }
       })
     } else {
-      util.showModel("提示","存在隐患时须提交完整检查报告！")
+      if (photo != "../../images/camera.png" && description != "" && description != null) {
+        var data = {
+          date: util.getDate(),
+          workshopId: getApp().globalData.workshopInfo.workshopId,
+          openId: getApp().globalData.userInfo.openId,
+          admin: 0,
+          checkpointId: that.data.checkpointInfo.checkpointId,
+          checkpointName: that.data.checkpointInfo.checkpointName,
+          error: that.data.errorIndex,
+          description: description,
+          photo: photo
+        }
+        inspect.inspect(data, function (res) {
+          if (res.status == 1) {
+            util.showModel("提示", "提交成功")
+            that.setData({
+              errorIndex: 0,
+              description: '',
+              photo: "../../images/camera.png",
+              initText: "请输入文本",
+              initTextValue: "",
+            })
+            that.refresh()
+          } else if (res.status == -1) {
+            util.showModel("提示", "提交失败，请重试！")
+          } else {
+            util.showModel("提示", "请求出错！")
+          }
+        })
+      } else {
+        util.showModel("提示", "存在隐患时须提交完整检查报告！")
+      }
     }
   },
   /**
